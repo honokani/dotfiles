314c8b3a3b1496b82df43b8484702941f8d5391d
honokani <honokani.kaoriga@gmail.com>
2021-06-20 00:51:40 +0900

    :hammer: Fix make zsh find pyenv-virtualenv

--- initialize_ubuntu.3.sh
+++ initialize_ubuntu.3.sh
@@ -4,6 +4,8 @@ sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install -y neovim
 
+eval "$(pyenv init -)"
+
 # make envs python2 and 3 for neovim
 pyenv install 2.7.15
 pyenv virtualenv 2.7.15 neovim2
@@ -11,12 +13,12 @@ pyenv activate neovim2
 pip install --upgrade pip
 pip install neovim
 
-pyenv install 3.8.5
-pyenv virtualenv 3.8.5 neovim3
+pyenv install 3.9.5
+pyenv virtualenv 3.9.5 neovim3
 pyenv activate neovim3
 pip install --upgrade pip
 pip install neovim
 
 # install python3
 pip install --upgrade pip
-pyenv global 3.8.5
+pyenv global 3.9.5
