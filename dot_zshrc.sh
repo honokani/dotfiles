97d6f7348d0fd37d56603eaab80b52a1e765fb48
honokani <honokani.kaoriga@gmail.com>
2023-02-16 00:17:42 +0900

    feat: split zshrc into common and unique

--- dot_zshrc.sh
+++ dot_zshrc.sh
@@ -3,13 +3,15 @@ echo $SHELL
     if [[ "$(uname)" =~ 'Darwin' ]]; then
         CURR_OS='Mac'
         printf "Platform is ""$CURR_OS""."
-        . $HOME/.zshrc_for_mac
+        . "$HOME/.zshrc_for_common"
+        . "$HOME/.zshrc_for_mac"
         echo " zshrc has loaded. "
     elif [[ "$(uname)" =~ 'Linux' ]]; then
         if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
             CURR_OS='WindowsSubsystemLinux'
             printf "Platform is ""$CURR_OS""."
-            . $HOME/.zshrc_for_wsl
+            . "$HOME/.zshrc_for_common"
+            . "$HOME/.zshrc_for_wsl"
             echo " zshrc has loaded. "
         else
             CURR_OS='Linux not WSL'
