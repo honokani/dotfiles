1ed31b5ece66523071287c5f617ae8a0c95bc05b
honokani <honokani.kaoriga@gmail.com>
2025-06-28 00:42:16 +0900

    fix: pattern using case

--- dot_zshrc.sh
+++ dot_zshrc.sh
@@ -1,33 +1,41 @@
 : "Call_zsh_core_for_each_OS" && {
-    CURR_OS=""
     . "$HOME/.zshrc_util"
-    # . "$HOME/.zshrc_sandbox"
-    if [[ "$(uname)" =~ 'Darwin' ]]; then
-        CURR_OS="Mac"
-        . "$HOME/.zshrc_for_common"
-        . "$HOME/.zshrc_for_mac"
-    elif [[ "$(uname)" =~ 'Linux' ]]; then
-        if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
-            CURR_OS='WindowsSubsystemLinux'
+
+    CURR_OS=""
+    case "$(uname)" in
+        Darwin)
+            CURR_OS="Mac"
             . "$HOME/.zshrc_for_common"
-            . "$HOME/.zshrc_for_wsl"
-        else
-            CURR_OS="Linux not WSL"
+            . "$HOME/.zshrc_for_mac"
+        Linux)
+            if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+                CURR_OS='WSL on Windows'
+                . "$HOME/.zshrc_for_common"
+                . "$HOME/.zshrc_for_wsl"
+            else
+                CURR_OS="Linux not WSL"
+                . "$HOME/.zshrc_for_common"
+            fi
+            ;;
+        MINGW32_NT*)
+            CURR_OS="Windows 32bit"
+            . "$HOME/.zshrc_for_windows"
+            ;;
+        MINGW64_NT*)
+            CURR_OS="Windows 64bit"
+            . "$HOME/.zshrc_for_windows"
+            ;;
+        *)
             . "$HOME/.zshrc_for_common"
-        fi
-    elif [[ "$(uname)" =~ 'MINGW32_NT' ]]; then
-        CURR_OS="Windows 32bit"
-        . "$HOME/.zshrc_for_windows"
-    elif [[ "$(uname)" =~ 'MINGW64_NT' ]]; then
-        CURR_OS="Windows 64bit"
-        . "$HOME/.zshrc_for_windows"
-    fi
+            unique=""
+            ;;
+    esac
 
     if [ -z "$CURR_OS" ]; then
         echo "Platform is : ($(uname -a))"
     else
         echo "Platform is ""$CURR_OS"". "
-        echo "Here is on ""$SHELL"". And zshrc has loaded. "
     fi
+    echo "Here is on ""$SHELL"". And zshrc has loaded. "
 }
 
