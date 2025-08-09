2995d2550935fdc19329502e97051d8847322382
honokani <honokani.kaoriga@gmail.com>
2025-08-09 15:48:59 +0900

    Update initialize_ubuntu.1.sh
    fix: initial command for linux

--- initialize_ubuntu.1.sh
+++ initialize_ubuntu.1.sh
@@ -1,13 +1,18 @@
-# install zsh
-# install zplug
-# # see https://github.com/zplug/zplug
-sudo apt install -y zsh
-sudo apt install -y curl
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-# change shell
-# $ chsh
-which zsh
-sudo chsh -s $(which zsh) $USER
+#!/usr/bin/zsh
+# install zsh (冪等性対応)
+if ! command -v zsh &> /dev/null; then
+    sudo apt update
+    sudo apt install -y zsh curl
+else
+    echo "zsh is already installed"
+fi
+
+# change shell (現在のシェルがzshでない場合のみ)
+if [[ "$SHELL" != "$(which zsh)" ]]; then
+    sudo chsh -s $(which zsh) $USER
+    echo "Shell changed to zsh. Please reboot and run initialize_ubuntu.2.sh"
+else
+    echo "Already using zsh"
+fi
 
 echo do_reboot!!
-
